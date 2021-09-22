012954f6b171c1d35e224482cc5ca39687aa4d0a
xDave12x <77081815+xDave12x@users.noreply.github.com>
2021-09-22 16:37:41 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function preload(){
+
+}
+function setup(){
+canvas=createCanvas(300,300);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/aM2VEWD6f/model.json',modelLoaded);
+}
+function modelLoaded(){
+    console.log("model loaded");
+}
+function draw(){
+image(video,0,0,300,300);
+classifier.classify(video,gotResult);
+}
+function gotResult(error,results){
+if(error){
+    console.error(error);
+}
+else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML=results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+}
+}
